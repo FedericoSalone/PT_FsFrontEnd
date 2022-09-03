9bb9f6a8a1219ab3869cb7d029353d6a8c9fad8e
FedericoSalone <fede.salone19@gmail.com>
2022-09-03 00:15:58 -0300

    FrontEnd TP
    Front End de Trabajo Integrador

---
+++ PortfolioFinal/FrontEnd/src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { BannerComponent } from './componentes/banner/banner.component';
+import { InfopersonalComponent } from './componentes/infopersonal/infopersonal.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { HardAndSkillsComponent } from './componentes/hard-and-skills/hard-and-skills.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    BannerComponent,
+    InfopersonalComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    HardAndSkillsComponent,
+    ProyectosComponent,
+    FooterComponent,
+   
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
