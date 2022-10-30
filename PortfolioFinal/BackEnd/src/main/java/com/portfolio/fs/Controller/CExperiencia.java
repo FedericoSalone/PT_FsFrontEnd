934ca319582682ecefed2c95bc5389ad3a7b4dcd
FedericoSalone <fede.salone19@gmail.com>
2022-10-30 19:48:59 -0300

    corrigiendo back
    problemas con el postman

--- PortfolioFinal/BackEnd/src/main/java/com/portfolio/fs/Controller/CExperiencia.java
+++ PortfolioFinal/BackEnd/src/main/java/com/portfolio/fs/Controller/CExperiencia.java
@@ -23,8 +23,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/explab")
-@CrossOrigin(origins = "http://localhost:4200")
-//@CrossOrigin(origins = "https://fsfrontend.web.app")
+//@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://fsalonefrontend.web.app")
 public class CExperiencia {
     @Autowired
     SExperiencia sExperiencia;
